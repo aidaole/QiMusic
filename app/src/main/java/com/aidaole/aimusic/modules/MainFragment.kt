acb54eea843fd6a50e8b9d09e10fe9a452f40b8a
aidaole <aidaole@outlook.com>
2023-04-05 17:30:26 +0800

    调整首页tab

--- app/src/main/java/com/aidaole/aimusic/modules/MainFragment.kt
+++ app/src/main/java/com/aidaole/aimusic/modules/MainFragment.kt
@@ -5,12 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.aidaole.aimusic.databinding.FragmentMain2Binding
 import com.aidaole.aimusic.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
-    private val layout by lazy { FragmentMain2Binding.inflate(layoutInflater) }
+    private val layout by lazy { FragmentMainBinding.inflate(layoutInflater) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
