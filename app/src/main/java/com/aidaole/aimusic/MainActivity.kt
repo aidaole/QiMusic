e75d75048c1d8bf27dbcfb86c518b978b6733dd2
aidaole <aidaole@outlook.com>
2023-07-01 07:45:39 +0800

    适配透明状态栏

--- app/src/main/java/com/aidaole/aimusic/MainActivity.kt
+++ app/src/main/java/com/aidaole/aimusic/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.aidaole.aimusic
 
+import android.os.Bundle
+import android.view.ViewGroup.LayoutParams
 import androidx.activity.viewModels
 import com.aidaole.aimusic.databinding.ActivityMainBinding
 import com.aidaole.aimusic.framework.ViewBindingActivity
@@ -11,4 +13,24 @@ class MainActivity : ViewBindingActivity<ActivityMainBinding>() {
     private val playingVm by viewModels<PlayMusicViewModel>()
 
     override fun getViewBinding(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fitStatusBarHeight()
+    }
+
+    private fun fitStatusBarHeight() {
+        layout.rootLayout.setPadding(0, statusBarHeight(), 0, 0)
+    }
+
+    private fun statusBarHeight(): Int {
+        var height = 0
+        val resourceId = applicationContext.resources.getIdentifier("status_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            height = applicationContext.resources.getDimensionPixelSize(resourceId)
+        } else {
+            height = resources.getDimensionPixelSize(R.dimen.default_statusbar_height)
+        }
+        return height
+    }
 }
