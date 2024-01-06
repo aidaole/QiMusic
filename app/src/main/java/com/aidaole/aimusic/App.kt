4ac10fe3209a7c392b04a33ad5732c648526637a
aidaole <aidaole@outlook.com>
2024-01-06 19:56:57 +0800

    添加appviewmodel获取方法

--- app/src/main/java/com/aidaole/aimusic/App.kt
+++ app/src/main/java/com/aidaole/aimusic/App.kt
@@ -6,21 +6,32 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Handler
 import android.view.WindowManager
+import androidx.lifecycle.HasDefaultViewModelProviderFactory
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelLazy
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStore
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
-import okhttp3.*
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import java.io.IOException
 import javax.inject.Inject
 
 @HiltAndroidApp
-class App : Application() {
+class App : Application(), HasDefaultViewModelProviderFactory {
 
     @Inject
     lateinit var okHttpClient: OkHttpClient
     private lateinit var mainHandler: Handler
 
+    val viewModelStore = ViewModelStore()
+    lateinit var viewModelFactory: ViewModelProvider.Factory
 
     companion object {
         private lateinit var appContext: App
@@ -66,4 +77,15 @@ class App : Application() {
     fun runMainThread(call: () -> Unit) {
         mainHandler.post(call)
     }
+
+    override fun getDefaultViewModelProviderFactory(): ViewModelProvider.Factory {
+        if (viewModelFactory == null) {
+            viewModelFactory = ViewModelProvider.AndroidViewModelFactory.getInstance(this)
+        }
+        return viewModelFactory
+    }
 }
+
+internal inline fun <reified VM : ViewModel> appViewModels() = ViewModelLazy(VM::class,
+    { App.get().viewModelStore },
+    { App.get().viewModelFactory })
